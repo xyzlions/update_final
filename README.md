f7cadb94d8624e9a8bb4aa6a38dc8ac1bc51fc24
xyzlions <47937465+xyzlions@users.noreply.github.com>
2019-06-01 01:00:11 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# edd_demo
+index.html 首頁<Br>
+about_us 關於我們<Br>
+allproduct 商品總覽<Br>
+product_info 商品資訊<Br>
+cart 購物車<Br>
+member 會員中心<Br>
---
+++ assets/js/memberbutton.js
@@ -0,0 +1,28 @@
+
+// 跳出框
+var modal = document.getElementById('change_file');
+
+//點空白關閉
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+//跳出框
+var modal = document.getElementById('order_list');
+
+// 點空白關閉
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+// 跳出框
+var modal = document.getElementById('comment');
+
+// 點空白關閉
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
